1aea15fa5fbe99dba38b56acd2b2c3eddac102fe
Hossein Mohammadi <hosseinm.developer@gmail.com>
2022-10-22 12:32:49 +0330

    add receiving details adapter

---
+++ app/src/main/java/com/wms/wms/ui/receivingDetails/ItemsViewModel.kt
@@ -0,0 +1,4 @@
+package com.wms.wms.ui.receivingDetails
+
+data class ItemsViewModel(val image: Int, val text: String) {
+}
--- app/src/main/java/com/wms/wms/ui/receivingDetails/ReceivingDetailsFragment.kt
+++ app/src/main/java/com/wms/wms/ui/receivingDetails/ReceivingDetailsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.wms.wms.R
 import com.wms.wms.databinding.FragmentReceivingDetailsBinding
 
 /**
@@ -31,6 +33,26 @@ class ReceivingDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // getting the recyclerview by its id
+        val recyclerview = _binding!!.recyclerview
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(context)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ItemsViewModel(R.drawable.ic_baseline_input_24, "Item " + i))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = DetailsAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
 //        binding.buttonFirst.setOnClickListener {
 //            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
 //        }
